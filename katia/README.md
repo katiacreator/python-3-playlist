ca1fdec6d64d8753763e0cd2911513abc764e04b
katiacreator <katia.creator@gmail.com>
2021-08-22 17:16:13 -0400

    created lessons and projects dirs and completed circle area calc proj from lesson 6 video

--- katia/README.md
+++
@@ -1,3 +0,0 @@
-# python-3-playlist
-Course files for the Python 3 tutorial series from The Net Ninja YouTube channel
-bfdvchgfhgfhgfhfghggf
---
+++ katia/projects/area_calc.py
@@ -0,0 +1,14 @@
+# baby's first python code
+# input takes in data and print sends out data
+# For example, print("Today is Sunday") prints out today is sunday in the terminal
+
+# name = input("Tell me your name:")
+# age = input('...and your age:')
+# print(name, 'you are' + age )
+
+# Calculate the area of a circle
+# pi r**2
+
+radius = input('Enter the radius of your circle(m): ')
+area = 3.142 * int(radius) **2 #int turns a str into a num
+print('The area of your circle is:', area)
